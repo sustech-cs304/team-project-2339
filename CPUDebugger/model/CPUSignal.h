80a77a834d4e75200efe8bc925da0bc2756bfc26
David Li <45333770+iDavid2619@users.noreply.github.com>
2023-04-12 23:32:14 +0800

    Removed Signal's type which can be reallocated to Module class

--- CPUDebugger/model/CPUSignal.h
+++ CPUDebugger/model/CPUSignal.h
@@ -10,9 +10,6 @@ private:
     size_t    width;        // the width after byte alignment
     size_t    rawWidth;     // the original width without byte alignment
     QBitArray bitVal;       // value of the signal
-    enum SignalType {
-        INPUT = 1, OUTPUT = 0
-    }         type;         // the type of the CPU's signal (input or output)
     bool      isFlagged;
 };
 
