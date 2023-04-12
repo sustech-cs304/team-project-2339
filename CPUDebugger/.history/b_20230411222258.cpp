31005d1f294581432693c6069c182f3d2ca3b167
chenrrry <wonu19960717@163,com>
2023-04-12 20:26:07 +0800

    fix page2 add listview

--- CPUDebugger/.history/b_20230411222258.cpp
+++ CPUDebugger/.history/b_20230411222258.cpp
@@ -45,35 +45,3 @@ int main()
     qDebug() << DebugStore::asmCurLine;
 
 }
-
-
-
-#include <QGuiApplication>
-#include <QQmlApplicationEngine>
-#include <QQmlContext>
-#include <QScreen>
-#include "myobject1.h"
-
-int main(int argc, char *argv[])
-{
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-#endif
-    QGuiApplication app(argc, argv);
-
-    QQmlApplicationEngine engine;
-
-    QQmlContext* context = engine.rootContext();
-//    QScreen *screen = QGuiApplication::primaryScreen();
-//    context->setContextProperty("MyObject1",MyObject1::getInstance());
-    qmlRegisterType<MyObject1>("MyObj",1,0,"MyObject1");
-    const QUrl url(QStringLiteral("qrc:/main.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
-    engine.load(url);
-
-    return app.exec();
-}
--- CPUDebugger/view/myobject1.h
+++ CPUDebugger/view/myobject1.h
@@ -12,6 +12,10 @@
 #include "Stores/PreDebugStore.h"
 #include "Stores/DebugStore.h"
 
+#include <QFile>
+#include <QTextStream>
+#include <QVariantList>
+
 class MyObject1 : public QObject
 {
     Q_OBJECT
@@ -30,6 +34,8 @@ public:
 
     Q_INVOKABLE void getAsmFile();
 
+    Q_INVOKABLE void loadFile1();
+
     Q_INVOKABLE void makeBreakPoint();
 
     Q_INVOKABLE void asmStep();
@@ -41,22 +47,33 @@ public:
     int value1() const;
     void setValue1(int newValue1);
 
+    QVariantList items1() const;
+    void setItems1(const QVariantList &newItems1);
+
 private:
     int m_value;
     int m_value1;
     QString m_string;
     std::shared_ptr<QFile> file1;
+    QVariantList m_items1;
+
+
     Q_PROPERTY(int value READ value WRITE setValue NOTIFY valueChanged)
 
     Q_PROPERTY(QString string READ string WRITE setString NOTIFY stringChanged)
 
     Q_PROPERTY(int value1 READ value1 WRITE setValue1 NOTIFY value1Changed)
 
+    Q_PROPERTY(QVariantList items1 READ items1 WRITE setItems1 NOTIFY items1Changed)
+
 signals:
 
     void valueChanged();
     void stringChanged();
     void value1Changed();
+    void items1Changed();
 };
 
 #endif // MYOBJECT1_H
+
+
