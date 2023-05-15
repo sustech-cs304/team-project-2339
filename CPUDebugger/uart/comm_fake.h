b2ef0efc0446e856ad8e8a515cb61d37c8a8290a
Faccococo <105764948+Faccococo@users.noreply.github.com>
2023-05-15 12:25:00 +0800

    feat(comm_fake.h): add fake uart
    (Qt Creator in MacOS is quite a bully shit

--- CPUDebugger/uart/comm_fake.h
+++ CPUDebugger/uart/comm_fake.h
@@ -1,14 +1,25 @@
-
 #ifndef COMM_FAKE_H
 #define COMM_FAKE_H
 
+#include "QSerialPort"
+#include "QSerialPortInfo"
+#include "QObject"
 
 
 
-class comm_fake
+class UartCommunicator_fake : public QObject
 {
 public:
-    comm_fake();
-};
+    explicit UartCommunicator(QObject *parent = nullptr);
+    ~UartCommunicator();
+    bool connectCPU(const QString &portName, double packetWaitingSeconds=1.0, double totWaitingSeconds=3.0, int pingNum=3);
+    bool autoConnectCPU(double packetWaitingSeconds=1.0, double totWaitingSeconds=3.0, int pingNum=3);
+    bool sendResume(QByteArray& cpuResponse, int nextPC, double packetWaitingSeconds=1.0, double totWaitingSeconds=1.5);
+    bool sendPausePacket(QByteArray& cpuResponse, double packetWaitingSeconds=1.0, double totWaitingSeconds=1.5);
+    void sendPause();
+    bool sendStep(QByteArray& cpuResponse, double packetWaitingSeconds=1.0, double totWaitingSeconds=1.5);
+    bool sendProgram(const QByteArray& asmFile, double packetWaitingSeconds=2.0, double totWaitingSeconds=3.0);
 
+    void setBaudRate(int baudRate);
+}
 #endif // COMM_FAKE_H
